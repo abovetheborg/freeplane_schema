06c9898d2b2d19a874a5b0f9cff6c49179ddc4b4
Michel JACQUES <mijacques@tesla.com>
2018-12-31 11:03:43 -0500

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='freeplane_schema',
+    version='0.1.0',
+    description='Python representation of the Freeplane XML schema',
+    long_description=readme,
+    author='Michel JACQUES',
+    author_email='mmichelm.jacques@gmail.com',
+    url='https://github.com/abovetheborg/freeplane_schema',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
